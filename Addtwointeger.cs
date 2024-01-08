347e762c0f56e8fad0a19ab10741381bc63270c2
Laxman <laxmanbinekar98@gmail.com>
2024-01-08 22:28:58 +0530

    My First Commit

---
+++ Addtwointeger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPractice
+{
+    internal class Addtwointeger
+    {
+        public void AddNumber()
+        {
+            int a, b, result;
+
+            Console.WriteLine("Enter First Number :");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Second Number : ");
+            b = int.Parse(Console.ReadLine());
+
+            result = a + b;
+
+            Console.WriteLine("Addition is {0}", result);
+            
+
+            int yearofexperience = 2;
+            string name = "Trupti shinde";
+            string Destination = "web Devloper";
+            string company = "Aarohi";
+
+            Console.WriteLine("My experience is {0},My destination is {2} and My name is {1},my ComPany NAme is {3}", yearofexperience, name, Destination, company);
+            
+        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMenu();
+            ExecutedMenuOption();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ReadLine();
+        }
+
+        public static void ExecutedMenuOption()
+        {
+            Console.Write("Enter Your Choice :");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Addtwointeger adding = new Addtwointeger();
+                    adding.AddNumber();
+                break;
+
+                case 2:
+                ControlStatement control = new ControlStatement();
+
+                break;
+            }
+            Console.ReadLine();
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine("____________Menu__________");
+            Console.WriteLine("1.Add Two Integer");
+        }
+
+    }
+}
